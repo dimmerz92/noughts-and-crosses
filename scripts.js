ff4705016db00f1945b557a186351271693ac6aa
dimmerz92 <andrew.weymes@outlook.com>
2022-09-21 09:53:02 +0800

    add player detection and game stop functionality

--- scripts.js
+++ scripts.js
@@ -1,16 +1,26 @@
 const Player = (marker) => {
+    let wins = 0;
 
     function getMarker() {
         return marker;
     };
 
-    return {getMarker}
+    function winGame() {
+        wins++;
+    };
+
+    function getWins() {
+        return wins;
+    };
+
+    return {getMarker, winGame, getWins}
 };
 
 const gameBoard = (() => {
-    // Constants
+    // Attributes
     const board = Array(9).fill(null);
     const tiles = [...document.getElementById("board").children];
+    let gameStatus = true;
 
     // Listeners
     tiles.forEach(tile => {tile.addEventListener("click", tile => updateBoard(Number(tile.target.id) - 1))});
@@ -25,32 +35,48 @@ const gameBoard = (() => {
     function reset() {
         board.fill(null);
         render();
+        gameStatus = true;
     };
 
-    function checkWins() {
+    function stopGame() {
+        gameStatus = false;
+    };
+
+    function checkWins(marker) {
         const patterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-        let flag = false;
 
         for (let i = 0; i < patterns.length; i++) {
             let [a,b,c] = [patterns[i][0], patterns[i][1], patterns[i][2]];
-            if (board[a] && board[b] && board[c]) {
-                flag = true;
+            if (board[a] === marker && board[b] === marker && board[c] === marker) {
+                return "win";
+            } else if (board.length === 9 && !board.includes(null)) {
+                return "draw";
             };
         };
-        return flag;
     };
 
     function updateBoard(tile) {
+        if (!gameStatus) {
+            return;
+        };
+
+        let marker = gameController.whosTurn();
+
         if (board[tile]) {
             console.log("something already here");
         } else {
-            board[tile] = gameController.whosTurn();
+            board[tile] = marker;
             render();
-            return checkWins();
         }
+
+        if (checkWins(marker) === "win") {
+            gameController.endGame(marker);
+        } else if (checkWins(marker) === "draw") {
+            gameController.endGame();
+        };
     };
 
-    return {reset};
+    return {reset, stopGame};
 
 })();
 
@@ -69,6 +95,19 @@ const gameController = (() => {
         };
     };
 
-    return {whosTurn}
+    function endGame(marker = null) {
+        // TODO
+        if (player1.getMarker() === marker) {
+            player1.winGame();
+            gameBoard.stopGame();
+        } else if (player2.getMarker() === marker) {
+            player2.winGame();
+            gameBoard.stopGame();
+        } else {
+            console.log(marker);
+        };
+    };
+
+    return {whosTurn, endGame, player1, player2}
 })();
 
